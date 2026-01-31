a1e59406375e2f0f0552f8a7b1c851177c878b0b
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2026-01-31 18:35:47 +0900

    패킷 수신시 예외를 띄우지 않음

--- Server/PacketHandler.cs
+++ Server/PacketHandler.cs
@@ -14,37 +14,37 @@ namespace Shared.Contents
 
         public void C_BuyShopItemReq_Handle(Session session, C_BuyShopItemReq packet)
         {
-            throw new NotImplementedException();
+
         }
 
         public void C_ResourceInfoReq_Handle(Session session, C_ResourceInfoReq packet)
         {
-            throw new NotImplementedException();
+
         }
 
         public void C_ResourceInfoReq_Handler(Session session, C_ResourceInfoReq packet)
         {
-            throw new NotImplementedException();
+
         }
 
         public void C_ShopInfoReq_Handle(Session session, C_ShopInfoReq packet)
         {
-            throw new NotImplementedException();
+
         }
 
         public void S_BuyShopItemRes_Handle(Session session, S_BuyShopItemRes packet)
         {
-            throw new NotImplementedException();
+
         }
 
         public void S_ResourceInfoRes_Handle(Session session, S_ResourceInfoRes packet)
         {
-            throw new NotImplementedException();
+
         }
 
         public void S_ShopInfoRes_Handle(Session session, S_ShopInfoRes packet)
         {
-            throw new NotImplementedException();
+
         }
     }
 }
