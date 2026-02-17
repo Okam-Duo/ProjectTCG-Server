63587ccbd81d16e6bd3db79dc420aae7e8f8dce6
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2026-02-18 06:50:01 +0900

    계정 서버에 로그인, 회원가입용 sql 추가

--- Server/AccountServer.cs
+++ Server/AccountServer.cs
@@ -12,6 +12,7 @@ namespace Server
     {
         private Listener<ClientSession> _listener;
         private AccountServerPacketHandler _packetHandler;
+        private DBConnection _db;
 
         public AccountServer()
         {
@@ -28,6 +29,7 @@ namespace Server
                 sessionFactory: () => { return new ClientSession(_packetHandler); },
                 onAcceptAsync: OnAcceptAsync
                 );
+            _db = new DBConnection();
 
 
             _listener.StartListen();
@@ -35,9 +37,78 @@ namespace Server
             Console.WriteLine($"{nameof(AccountServer)} is constructed!");
         }
 
+        public async Task<bool> CheckIdAvailable(string loginId)
+        {
+            bool available = false;
+
+            await _db.RunSql(
+                @"SELECT count(*) as idCount
+FROM Accounts
+WHERE loginId = @ID;",
+                (reader) =>
+                {
+                    reader.Read();
+                    int idCount = (int)reader["idCount"];
+                    if (idCount == 0)
+                    {
+                        available = true;
+                    }
+                },
+                new() { { "@ID", loginId } }
+                );
+
+            return available;
+        }
+
+        public async Task<bool> TrySignIn(string loginId, string passwordHash, string nickName)
+        {
+            bool isSuccess;
+            Exception? e;
+
+            (isSuccess, e) = await _db.RunSql(
+                @"INSERT INTO Accounts(loginId,loginPasswordHash,nickName)
+VALUES(@id,@password,@nickname);",
+                callBack: null,
+                new() { { "@id", loginId }, { "@password", passwordHash }, { "@nickname", nickName } }
+                );
+
+            return isSuccess;
+        }
+
+        //반환 : uid, -1이라면 로그인 실패
+        public async Task<int> TryGetUID(string loginId, string passwordHash)
+        {
+            int userId = -1;
+
+            await _db.RunSql(
+                @"BEGIN TRAN;
+
+	UPDATE Accounts
+	SET lastLoginDateTime = CURRENT_TIMESTAMP
+	WHERE loginId=@ID;
+
+	SELECT *
+	FROM Accounts
+	WHERE loginId=@ID;
+
+COMMIT;",
+                (reader) =>
+                {
+                    reader.Read();
+                    if (passwordHash == (string)reader["loginPasswordHash"])
+                    {
+                        userId = (int)reader["userId"];
+                    }
+                },
+                new() { { "@ID", loginId } }
+                );
+
+            return userId;
+        }
+
         private async void OnAcceptAsync(ClientSession session)
         {
-            
+
         }
     }
 }
