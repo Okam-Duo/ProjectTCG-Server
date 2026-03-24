acc8402a86921a850661829a82756f0b678506f0
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2026-03-24 11:56:45 +0900

    ID 생성기 구현

---
+++ Server/IdGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTCG_Server.Server
+{
+    public class IdGenerator
+    {
+        private int _nextId;
+
+        public int GenerateNewId()
+        {
+            return _nextId++;
+        }
+    }
+}
